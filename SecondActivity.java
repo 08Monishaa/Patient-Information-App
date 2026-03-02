09f74e3bbb6b204db71cd71d295d51021441540b
MONISHA V <velusamymonisha438@gmail.com>
2026-03-02 15:45:35 +0530

    Create SecondActivity.java

---
+++ SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.patientinformationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Patient Details - REG NO: 23ITXXX");
+        setContentView(R.layout.activity_second);
+    }
+}
